08e85efc8e0ec86fada29c88e555d6e71c189502
likhitha <likhitha0341@gmail.com>
2023-12-26 01:17:51 +0530

    click me

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,20 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 function App() {
   const [text,setText]=useState("");
+  const getData=
+  async () => {
+      
+    const response = await axios.get("http://localhost:5000/api/get");
+    setText(response.data.message);
+
+}
   useEffect(() => 
-    async () => {
-      
-          const response = await axios.get("http://localhost:5000/api/get");
-          setText(response.data.message);
-      
-  }
-  , );
+   
+  {} );
   return (
     <div className="App">
       <header className="App-header">
+        <h1 onclick={getData}>CLICK ME</h1>
        <h1>{text}</h1>
       </header>
     </div>
